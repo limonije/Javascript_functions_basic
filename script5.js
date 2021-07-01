28e4b7c80760924e8bfe4e0ce6ecfccde34ec1b5
limonije <mroozen@live.nl>
2021-07-01 17:02:43 +0200

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,27 @@
+const makePizza = function (){
+    console.log("Maak deeg");
+    console.log("Maak tomatensaus");
+    console.log("Verwarm de oven");
+    console.log("Rol het deeg uit op het bakblik");
+    console.log("Verdeel de tomatensaus over het deeg");
+    console.log("Beleg de pizza met salami en groenten");
+    console.log("Bak de pizza 10 minuten");
+};
+
+const makeSushi = function (){
+  console.log("Kook sushirijst");
+  console.log("Snij tonijn in repen");
+  console.log("Snij komkommer in repen");
+  console.log("Leg de bamboe mat klaar");
+  console.log("Leg het nori vel op de matmet de gladde kant naar de onder");
+  console.log("Beleg met sushirijst, tonijn en komkommer");
+  console.log("Maak een rol en snij in stukjes");
+  console.log("Snij de rol in plakjes");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
